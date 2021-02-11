04a75881e8d85b66147e494e6899b01ab6676683
MarcoGrimaldo <voskogrim@gmail.com>
2021-02-10 18:40:16 -0600

    making router work

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import M from "materialize-css";
-import Image1 from '../../Images/3.jpg';
+import Link from 'react-router-dom';
 
 export default class Navbar extends Component {
     componentDidMount() {
@@ -13,25 +13,25 @@ export default class Navbar extends Component {
             <div>
                 <nav className="blue-grey darken-4">
                     <div className="nav-wrapper container">
-                    <a href="/" className="brand-logo"><h5>{'RecorreTeotihuacán'}</h5></a>
-                    <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                    <Link href="/" className="brand-logo"><h5>{'RecorreTeotihuacán'}</h5></Link>
+                    <Link href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></Link>
                     <ul className="right hide-on-med-and-down">
-                        <li><a href="/">{'Inicio'}</a></li>
-                        <li><a href="/reserva">{'Reserva'}</a></li>
-                        <li><a href="/contacto">{'Contacto'}</a></li>
+                        <li><Link href="/">{'Inicio'}</Link></li>
+                        <li><Link href="/reserva">{'Reserva'}</Link></li>
+                        <li><Link href="/contacto">{'Contacto'}</Link></li>
                     </ul>
                     </div>
                 </nav>
 
                 <ul className="sidenav grey lighten-3" id="slide-out">
                     {/*<li><img src={Image1}/></li>*/}
-                    <li><a href="/" className="blue-grey darken-4 white-text">{'Recorre Teotihuacán'}</a></li>
-                    <li><a href="/" className="waves-effect"><i className="material-icons">home</i>{'Inicio'}</a></li>
-                    <li><a href="/reserva" className="waves-effect"><i className="material-icons">bookmark_border</i>{'Reserva'}</a></li>
-                    <li><a href="/contacto" className="waves-effect"><i className="material-icons">place</i>{'Contacto'}</a></li>
+                    <li><Link href="/" className="blue-grey darken-4 white-text">{'Recorre Teotihuacán'}</Link></li>
+                    <li><Link href="/" className="waves-effect"><i className="material-icons">home</i>{'Inicio'}</Link></li>
+                    <li><Link href="/reserva" className="waves-effect"><i className="material-icons">bookmark_border</i>{'Reserva'}</Link></li>
+                    <li><Link href="/contacto" className="waves-effect"><i className="material-icons">place</i>{'Contacto'}</Link></li>
                     <li><div className="divider"></div></li>
-                    <li><a href="/contacto" className="waves-effect"><i className="material-icons">local_phone</i>{'+52 554826482'}</a></li>
-                    <li><a href="/contacto" className="waves-effect"><i className="material-icons">mail_outline</i>{'example@example.com'}</a></li>
+                    <li><Link href="/contacto" className="waves-effect"><i className="material-icons">local_phone</i>{'+52 554826482'}</Link></li>
+                    <li><Link href="/contacto" className="waves-effect"><i className="material-icons">mail_outline</i>{'example@example.com'}</Link></li>
 
                 </ul>
             </div>
