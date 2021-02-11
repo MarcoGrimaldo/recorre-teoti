7b1e4fbd3c839795d20619ee169a349dd18f5673
MarcoGrimaldo <voskogrim@gmail.com>
2021-02-10 18:42:18 -0600

    making router work

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import M from "materialize-css";
-import Link from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 export default class Navbar extends Component {
     componentDidMount() {
